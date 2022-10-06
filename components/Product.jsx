197e54c57272c9d6f2d970cd31faf88cf157b021
gitpk-0 <kell108p@protonmail.com>
2022-10-06 19:41:28 -0400

    update

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
-const Product = () => {
-  return <div>Product</div>;
+import { urlFor } from "../lib/client";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  // const
+
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            className="product-image"
+            width={250}
+            height={250}
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price} per lb</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
